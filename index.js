935f1f8961f08c77d91e74f7ea214f76f7d3e39f
utsabk <kharelutsab@gmail.com>
2021-08-11 09:19:26 +0300

    Frontend:  - forntend of the aplliccation added

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ const fetchUrl =
   'https://media.wizards.com/2021/downloads/MagicCompRules%2020210419.txt';
 
 
+
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 app.get('/', async (req, res) => {
 
   const response = await customFetch(fetchUrl);
@@ -27,6 +32,6 @@ app.get('/', async (req, res) => {
   res.json(group);
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('listening on port:', process.env.PORT);
 });
